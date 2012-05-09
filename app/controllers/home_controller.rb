12d0bc6bcc8b16af6efb3e26c2fb432d6a6562be
somuchbetter <dobri87@gmail.com>
2012-05-09 11:20:05 +0300

    [fix] example.json missing

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,6 @@ class HomeController < ApplicationController
 	def index
 		authorize = UrlShortener::Authorize.new 'o_6bp01kelcc', 'R_a8f8fd04d722529dcdab231222ee783a'
 		@client = UrlShortener::Client.new(authorize)
-		@list = ActiveSupport::JSON.decode(File.read("#{Rails.root}/tmp/example.json"))
+		@list = ActiveSupport::JSON.decode(File.read("#{Rails.root}/public/example.json"))
 	end
 end
